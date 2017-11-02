ca0201123a312658696554cda33794e820ff3ef7
faithgiguere <fgiguere@gwu.edu>
2017-11-02 09:00:35 -0400

    Layout setup

---
+++ ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Faith Giguere on 10/31/17.
+//  Copyright © 2017 Faith Giguere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: "This is my app!",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(slider.value)")
+    }
+}
+
