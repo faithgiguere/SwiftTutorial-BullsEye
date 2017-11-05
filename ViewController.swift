6872d8ec1dec6c13d5026c3502c3ae2d3571a918
faithgiguere <fgiguere@gwu.edu>
2017-11-05 00:18:03 -0400

    Created rounds and scoring, all labels now reflect their current values

--- ViewController.swift
+++ ViewController.swift
@@ -2,21 +2,23 @@
 //  ViewController.swift
 //  BullsEye
 //
-//  Created by Faith Giguere on 10/31/17.
-//  Copyright © 2017 Faith Giguere. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-    var currentValue: Int = 0
-    var targetValue: Int = 0
+    var currentValue = 0 // Current value of the slider
+    var targetValue = 0 // Target number to match with the slider
+    var score = 0 // Total cumulative score from all rounds
+    var round = 0
+    
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
-        targetValue = 1 + Int(arc4random_uniform(100))
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,8 +27,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)" +
-                      "\nThe target value is: \(targetValue)"
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        score += points
+        let message = "You hit: \(currentValue)\n" +
+                      "This round's score: \(points)"
         
         let alert = UIAlertController(title: "Hello, World",
                                       message: message,
@@ -37,10 +42,28 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    // For each new round, generates a new random number, resets the slider,
+    //and updates the score and target labels.
+    func startNewRound () {
+        round += 1
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    // Updates the label values of target and score.
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 }
 
