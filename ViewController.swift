d706a48c8c8dd77af9761b6d4ff86a871fba9c0c
faithgiguere <fgiguere@gwu.edu>
2017-11-05 13:57:06 -0500

    Next round does not update until OK is clicked, Start Over button starts a new game

--- ViewController.swift
+++ ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        startNewRound()
+        startNewGame()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,21 +28,33 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert() {
         let difference = abs(targetValue - currentValue)
-        let points = 100 - difference
+        var points = 100 - difference
         score += points
+        
+        // Messages correspond with general accuracy of user's attempt
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
         let message = "You hit: \(currentValue)\n" +
                       "This round's score: \(points)"
         
-        let alert = UIAlertController(title: "Hello, World",
+        let alert = UIAlertController(title: title,
                                       message: message,
                                       preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default, handler: {action in self.startNewRound()})
         
         alert.addAction(action)
-        
         present(alert, animated: true, completion: nil)
-        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
@@ -64,6 +76,11 @@ class ViewController: UIViewController {
         targetLabel.text = String(targetValue)
         scoreLabel.text = String(score)
         roundLabel.text = String(round)
+    }
+    @IBAction func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
     }
 }
 
